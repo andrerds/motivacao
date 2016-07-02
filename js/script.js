d9c57c52749cfac746fa6bfe8fa9588925396346
Matheus Alves <theuves@outlook.com>
2016-07-01 22:52:34 -0400

    pequenas alterações

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var texto = frases[Math.floor(Math.random() * frases.length)];
 
-var frase = texto.split('–')[0].trim();
-var autor = texto.split('–')[1].trim();
+var frase = '<b>“' + texto.split('–')[0].trim() + '”</b>';
+var autor = '<i>–' + texto.split('–')[1].trim() + '</i>';
 
 document.querySelector('.frase').innerHTML = frase;
 document.querySelector('.autor').innerHTML = autor;
